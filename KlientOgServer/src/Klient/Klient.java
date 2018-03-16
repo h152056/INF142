44dfd4d69bf0a445c27a52e816d7e4e99248ee3c
Petter A. Salberg <petter_salberg@hotmail.com>
2018-03-16 17:24:56 +0100

    Funker med UDP, implementer TCP for WPS

--- KlientOgServer/src/Klient/Klient.java
+++ KlientOgServer/src/Klient/Klient.java
@@ -15,12 +15,12 @@ public class Klient {
     */
 
     //private Socket socket = null;
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         //Lager socket
         DatagramSocket klientSocket = new DatagramSocket();
 
         //Oversetter servernavn til IP adresse ved bruk av DNS
-        InetAddress IPAddress = InetAddress.getByName("");
+        InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
 
         //Lager inputtstrøm
         BufferedReader inFraBruker = new BufferedReader(new InputStreamReader(System.in));
@@ -28,31 +28,22 @@ public class Klient {
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
 
-        try {
-            String line = inFraBruker.readLine();
-        }catch (IOException j){
-            System.out.println(j);
-        }
-        sendData = line.getBytes();
+        String line = inFraBruker.readLine();
 
+        sendData = line.getBytes();
+        //Lager datagram (data, lengde, IP adr, port
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 5000);
-        try {
-            klientSocket.send(sendPacket);
-        }catch (IOException i){
-            System.out.println(i);
-        }
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length):
-        try{
-            klientSocket.receive(receivePacket);
-        }catch (IOException k){
-            System.out.println(k);
-        }
+        //Sender datagram til server
+        klientSocket.send(sendPacket);
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        //Les datagram fra server
+        klientSocket.receive(receivePacket);
 
         String modifisertSetning = new String(receivePacket.getData());
 
         System.out.println("FROM SERVER:" + modifisertSetning);
         klientSocket.close();
-
 
 
     }
--- KlientOgServer/src/WPS/WPS.java
+++ KlientOgServer/src/WPS/WPS.java
@@ -1,4 +1,33 @@
 package WPS;
+import java.io.*;
+import java.net.*;
+
 
 public class WPS {
+    public static void main(String[] args)throws Exception{
+        //Lager datagramsocket på port 5000
+        DatagramSocket serverSocket = new DatagramSocket(5000);
+
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+
+        while (true){
+            //Lager plass for motatt datagram
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            //Motta datagram
+            serverSocket.receive(receivePacket);
+
+            String line = new String(receivePacket.getData());
+
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedLine = line.toUpperCase();
+            System.out.println("Packet mottatt fra:" + IPAddress.getHostName());
+            System.out.println("Data motatt:" + line);
+            sendData = capitalizedLine.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
 }
